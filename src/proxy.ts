2c36c8b003e731cda1ce2fd18cfad4478b42af54
karan5772 <karankumar8239@gmail.com>
2026-05-06 23:16:56 +0530

    updated proxy.js

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,7 +6,7 @@ const isProtectedRoute = createRouteMatcher([
   '/voice(.*)'
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+export const proxy = clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
 });
 
